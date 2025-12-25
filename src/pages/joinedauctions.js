54342127bf36836f061fd58776281e0a60f6dd43
sainath reddy <116480684+sainath1569@users.noreply.github.com>
2025-12-25 14:59:25 +0530

    Update joinedauctions.js

--- src/pages/joinedauctions.js
+++ src/pages/joinedauctions.js
@@ -41,7 +41,7 @@ const JoinedAuctions = () => {
       setLoading(true);
       setError(null);
       
-      const response = await fetch(`https://ipl-server-dsy3.onrender.com/api/auction/joined?email=${encodeURIComponent(userEmail)}`, {
+      const response = await fetch(`https://ipl-server-lake.vercel.app/api/auction/joined?email=${encodeURIComponent(userEmail)}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
